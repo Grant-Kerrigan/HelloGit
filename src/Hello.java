c9814f1897d594737d0bb590a25f4f8bc255641f
grant <grant@Grants-Laptop.unl.edu>
2022-08-25 11:37:04 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/*
+ * @author: Grant Kerrigan
+ * @Date: 2022/08/24
+ * 
+ * @purpose: Use GitHub to collaborate with a classmate
+ */
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Grant Kerrigan");
+	}
+}
